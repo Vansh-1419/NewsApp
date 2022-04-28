35684e9cb15a1600ec67d5775b5ece5170911306
Vansh Tandon <Vanshtandon1423@gmail.com>
2022-04-28 22:28:11 +0530

    API Error Handeling left

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 "use strict";
-const maincontainer = document.querySelector(".channel");
 const display = document.querySelector(".display");
 const news = document.querySelector(".news");
 const image = document.querySelector(".imge");
+const content = document.querySelector(".content");
 const data = fetch(
   "https://newsapi.org/v2/top-headlines?country=in&apiKey=af0c13a26cfc413c95692d80981a4934"
 )
@@ -12,13 +12,20 @@ const data = fetch(
     const dished = `<a href="${data.articles[0].url}"><h4 class="displayhead">${data.articles[0].title}!</h4></a>`;
     display.insertAdjacentHTML("beforeend", dished);
     data.articles.forEach((d) => {
-      const img = `<img src="${d.urlToImage}" alt="" srcset="" class="img"></img>`;
+      const img = `
+      <div class="imges">
+      <a href="${d.url}"><img src="${d.urlToImage}" alt="${d.source.name}" srcset="" class="img"></img></a>
+      </div>`;
       const html = `
-       <h4 class="heading"></h4> 
-      <p class="newspara"></p>
-       <p class="source"></p>`;
+      <a href="${d.url}"><div class="con">
+      
+      <h3 class="heading">${d.title}!</h3>
+      <p class="newspara">${d.description}</p>
+       <p class="source">Source:${d.source.name}</p>
+       </div></a>`;
 
-      maincontainer.insertAdjacentHTML("beforeend", html);
-      news.insertAdjacentHTML("beforeend", img);
+      image.insertAdjacentHTML("beforeend", img);
+      content.insertAdjacentHTML("beforeend", html);
+      // news.insertAdjacentHTML("beforeend", imge);
     });
   });
